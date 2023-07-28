07b0e2888e361589b499943f5becdf425ce22db6
Etienne Jouvençon <e.jouvencon@attineos.com>
2023-07-28 18:58:09 +0200

    Correction init

--- DadAPI/urls.py
+++ DadAPI/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("pointers/", include("pointers.urls")),
     path('api/', include('image_api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
